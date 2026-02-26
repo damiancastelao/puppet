209dbb6f0b7306ac5f497095f48cd079538677a1
Damián Nogueiras Meléndez <damian@danielcastelao.org>
2026-02-26 11:19:16 +0100

    Actualizado paquetes necesarios

--- manifests/paquetes.pp
+++ manifests/paquetes.pp
@@ -1,28 +1,29 @@
-# actualizamos/upgrade diariamente
+# Paquetes necesarios
+
+# TODO actualizamos/upgrade diariamente
+# TODO actualizar el source list de apt en este fichero, para incluir instalaciones como code
 
 class remove_package {
       # remove a package
       package { ['docker-engine', 'docker.io', 'containerd', 'runc']:
               ensure => 'absent',
-          }
-}
-
-class snap_package {
-      package { ['code','chromium']:
-        ensure   => latest,
-        provider => snap,
       }
 }
 
 class necesarios_package {
-      package { ['wireshark','git', 'emacs', 'virtualbox', 'virtualbox-guest-additions-iso']:
-      	      ensure => installed,
-	      }
-      class { 'docker': 
-      	         version => 'latest',
-	    }
-      class { 'unattended_upgrades':
-                 origins => ['${distro_id}:${distro_codename}','${distro_id}:${distro_codename}-security','${distro_id} ${distro_codename}-updates'],
-                 remove_new_unused_deps => true,
-            }
+      package { ['wireshark','git', 'emacs', 'virtualbox', 'code']:
+              ensure => installed,}
+
 }
+class { 'docker':
+                 version => 'latest',
+            }
+#class { 'unattended_upgrades':
+#                 origins => ['${distro_id}:${distro_codename}','${distro_id}:${distro_codename#}-security','${distro_id}\
+ ${distro_codename}-updates'],
+#                 remove_new_unused_deps => true,
+#            }
+#}
+
+include remove_package
+include necesarios_package
